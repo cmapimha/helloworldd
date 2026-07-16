c76505904b1162831a0269ab3d313c091dafb910
cmapimha <cmapimha@gmail.com>
2026-07-16 04:11:22 -0400

    update targeted app

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(3000, '0.0.0.0', () => {
-  console.log("Server is running on port 3000");
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
 });
 
